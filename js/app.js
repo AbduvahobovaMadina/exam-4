ca33cae8f845d47d03b9e32d5aa80b24cb1d44d2
AbduvahobovaMadina <madinaabduvahobova565@gmail.com>
2024-06-27 17:05:07 +0500

    /

---
+++ js/app.js
@@ -0,0 +1,16 @@
+const navbar = document.querySelector(".navbar__collection")
+
+function toggleShow (){
+    navbar.classList.add("show")
+}
+
+
+
+    function hideSidebar (){
+const sidebar = document.querySelector('.navbar__collection')
+ navbar.classList.remove("show")
+        
+        sidebar.style.display = 'none'
+       
+    }
+
